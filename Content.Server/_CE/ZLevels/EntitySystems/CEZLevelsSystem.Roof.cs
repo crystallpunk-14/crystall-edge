cf0aac6791ba55ceaaddd302e56128ef81e32619
Ed <edwardxperia2000@gmail.com>
2025-10-24 11:45:37 +0300

    delete roofPlacer

--- Content.Server/_CE/ZLevels/EntitySystems/CEZLevelsSystem.Roof.cs
+++ Content.Server/_CE/ZLevels/EntitySystems/CEZLevelsSystem.Roof.cs
@@ -18,14 +18,12 @@ public sealed partial class CEZLevelsSystem
         {
             //Sync for map below
             SyncMapRoofs(belowMapUid.Value, ent);
-            SyncMapTiles(ent, belowMapUid);
         }
 
         if (TryMapUp(ent, out _, out var aboveMapUid))
         {
             //Sync for this map
             SyncMapRoofs(ent, aboveMapUid);
-            SyncMapTiles(aboveMapUid.Value, ent);
         }
     }
 
@@ -48,40 +46,6 @@ public sealed partial class CEZLevelsSystem
         while (enumerator.MoveNext(out var tileRef))
         {
             Roof.SetRoof((currentMapUid, currentMapGrid, currentRoof), tileRef.Value.GridIndices, !tileRef.Value.Tile.IsEmpty);
-        }
-    }
-
-    /// <summary>
-    /// Goes through all RoofPlacer on the map from the bottom and places tiles on this map if there are empty tiles.
-    /// </summary>
-    private void SyncMapTiles(EntityUid currentMapUid, EntityUid? belowMapUid = null)
-    {
-        if (!TryComp<MapGridComponent>(currentMapUid, out var currentMapGrid))
-            return;
-
-        if (belowMapUid is null && !TryMapDown(currentMapUid, out var belowMapId, out belowMapUid))
-            return;
-
-        if (!TryComp<MapGridComponent>(belowMapUid, out var belowMapGrid))
-            return;
-
-        var query = EntityQueryEnumerator<CEZLevelRoofPlacerComponent, TransformComponent>();
-        while (query.MoveNext(out var uid, out var roofPlacer, out var xform))
-        {
-            if (xform.MapUid != belowMapUid)
-                continue;
-
-            var indices = _map.CoordinatesToTile(currentMapUid,
-                currentMapGrid,
-                new MapCoordinates(_transform.GetWorldPosition(uid), _transform.GetMapId(currentMapUid)));
-
-            if (_map.TryGetTileRef(currentMapUid, currentMapGrid, indices, out var tileRef))
-            {
-                if (!tileRef.Tile.IsEmpty)
-                    return;
-            }
-
-            _map.SetTile((currentMapUid, currentMapGrid), indices, new Tile(Proto.Index(roofPlacer.Tile).TileId));
         }
     }
 }
--- Content.Shared/_CE/ZLevels/EntitySystems/CESharedZLevelsSystem.Roof.cs
+++ Content.Shared/_CE/ZLevels/EntitySystems/CESharedZLevelsSystem.Roof.cs
@@ -1,5 +1,4 @@
 using Content.Shared.Light.EntitySystems;
-using Robust.Shared.Map;
 using Robust.Shared.Map.Components;
 
 namespace Content.Shared._CE.ZLevels.EntitySystems;
@@ -7,11 +6,9 @@ namespace Content.Shared._CE.ZLevels.EntitySystems;
 public abstract partial class CESharedZLevelsSystem
 {
     [Dependency] protected readonly SharedRoofSystem Roof = default!;
-    [Dependency] private readonly ITileDefinitionManager _tile = default!;
     private void InitRoof()
     {
         SubscribeLocalEvent<CEZLevelMapComponent, TileChangedEvent>(OnTileChanged);
-        SubscribeLocalEvent<CEZLevelRoofPlacerComponent, MapInitEvent>(OnRoofPlacerMapInit);
     }
 
     private void OnTileChanged(Entity<CEZLevelMapComponent> ent, ref TileChangedEvent args)
@@ -27,48 +24,6 @@ public abstract partial class CESharedZLevelsSystem
         {
             //Update rooving below map
             Roof.SetRoof(belowMapUid.Value, change.GridIndices, !change.NewTile.IsEmpty);
-
-            //Ensure tile above RoofPlacer
-            if (change.NewTile.IsEmpty)
-            {
-                var anchoredEntitiesBelow = _map.GetAnchoredEntitiesEnumerator(belowMapUid.Value, belowMapGrid, change.GridIndices);
-                while (anchoredEntitiesBelow.MoveNext(out var anchoredUid))
-                {
-                    if (!TryComp<CEZLevelRoofPlacerComponent>(anchoredUid, out var roofPlacer))
-                        continue;
-
-                    _map.SetTile((ent.Owner, mapGrid), change.GridIndices, new Tile(Proto.Index(roofPlacer.Tile).TileId));
-                }
-            }
         }
-    }
-
-    private void OnRoofPlacerMapInit(Entity<CEZLevelRoofPlacerComponent> ent, ref MapInitEvent args)
-    {
-        RoomPlacerProcess(ent);
-    }
-
-    protected void RoomPlacerProcess(Entity<CEZLevelRoofPlacerComponent> ent)
-    {
-        var placerXform = Transform(ent);
-
-        if (placerXform.MapUid is null)
-            return;
-        if (!TryMapUp(placerXform.MapUid.Value, out var aboveMapId, out var aboveMapUid))
-            return;
-        if (!TryComp<MapGridComponent>(aboveMapUid, out var aboveMapGrid))
-            return;
-
-        var indices = _map.CoordinatesToTile(aboveMapUid.Value,
-            aboveMapGrid,
-            new MapCoordinates(_transform.GetWorldPosition(ent), aboveMapId.Value));
-
-        if (_map.TryGetTileRef(aboveMapUid.Value, aboveMapGrid, indices, out var tileRef))
-        {
-            if (!tileRef.Tile.IsEmpty)
-                return;
-        }
-
-        _map.SetTile((aboveMapUid.Value, aboveMapGrid), indices, new Tile(Proto.Index(ent.Comp.Tile).TileId));
     }
 }
