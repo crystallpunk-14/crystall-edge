1680905624a237e106fadbe19b2f4d5e1f35434c
Ed <edwardxperia2000@gmail.com>
2025-11-28 00:20:36 +0300

    RandomOtherJob

--- Content.Shared/_CE/Ambitions/Parsings/CEAmbitionParsing.cs
+++ Content.Shared/_CE/Ambitions/Parsings/CEAmbitionParsing.cs
@@ -3,8 +3,11 @@ using Content.Shared._CE.LockKey;
 using Content.Shared.Dataset;
 using Content.Shared.Destructible.Thresholds;
 using Content.Shared.Humanoid.Prototypes;
+using Content.Shared.Mind;
 using Content.Shared.Mobs.Components;
 using Content.Shared.Roles;
+using Content.Shared.Roles.Components;
+using Content.Shared.Roles.Jobs;
 using JetBrains.Annotations;
 using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
@@ -110,14 +113,29 @@ public sealed partial class RandomNumber : CEAmbitionParsing
     }
 }
 
-public sealed partial class RandomJob : CEAmbitionParsing
+public sealed partial class RandomOtherJob : CEAmbitionParsing
 {
     public override string? GetText(IEntityManager entManager, IPrototypeManager protoManager, IRobustRandom random, EntityUid? owner)
     {
         List<JobPrototype> all = new();
 
+        var mindSys = entManager.System<SharedMindSystem>();
+        var jobSys = entManager.System<SharedJobSystem>();
+
+        if (owner == null)
+            return null;
+
+        if (!mindSys.TryGetMind(owner.Value, out var mindId, out var mind))
+            return null;
+
+        if (!jobSys.MindTryGetJob(mindId, out var currentJob))
+            return null;
+
         foreach (var job in protoManager.EnumeratePrototypes<JobPrototype>())
         {
+            if (currentJob == job)
+                continue;
+
             if (!job.SetPreference)
                 continue;
 
