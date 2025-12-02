1686fa937eccafd21b222fbc64f4cba95c6a619c
Ed <edwardxperia2000@gmail.com>
2025-12-03 00:58:19 +0300

    thanks to Emo!

--- Content.Client/_CE/Blinking/CEBlinkingSystem.cs
+++ Content.Client/_CE/Blinking/CEBlinkingSystem.cs
@@ -1,3 +1,10 @@
+/*
+ * This file is sublicensed under MIT License
+ * https://github.com/space-wizards/space-station-14/blob/master/LICENSE.TXT
+ *
+ * Taken from https://github.com/EphemeralSpace/ephemeral-space/pull/335/files?notification_referrer_id=NT_kwDOBb-lNbQyMDgzMjQ4Nzk4Nzo5NjQ0NTc0OQ
+ */
+
 using Content.Shared._CE.Blinking;
 using Content.Shared.Humanoid;
 using Robust.Client.Animations;
--- Content.Shared/_CE/Blinking/CESharedBlinkingSystem.cs
+++ Content.Shared/_CE/Blinking/CESharedBlinkingSystem.cs
@@ -1,3 +1,10 @@
+/*
+ * This file is sublicensed under MIT License
+ * https://github.com/space-wizards/space-station-14/blob/master/LICENSE.TXT
+ *
+ * Taken from https://github.com/EphemeralSpace/ephemeral-space/pull/335/files?notification_referrer_id=NT_kwDOBb-lNbQyMDgzMjQ4Nzk4Nzo5NjQ0NTc0OQ
+ */
+
 using Content.Shared.Bed.Sleep;
 using Content.Shared.Mobs;
 using Robust.Shared.Random;
