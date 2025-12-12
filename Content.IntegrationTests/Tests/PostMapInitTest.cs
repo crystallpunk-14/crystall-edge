b86f0aa8c92ac6c4c35908669befcac20f93d704
Ed <edwardxperia2000@gmail.com>
2025-12-12 12:35:11 +0300

    Update PostMapInitTest.cs

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -87,23 +87,7 @@ namespace Content.IntegrationTests.Tests
         private static readonly string[] GameMaps =
         {
             "Dev",
-            //"TestTeg",
-            //"Fland",
-            //"Packed",
-            //"Bagel",
-            //"CentComm",
-            //"Box",
-            //"Marathon",
-            //"MeteorArena",
-            //"Saltern",
-            //"Reach",
-            //"Oasis",
-            //"Amber",
-            //"Plasma",
-            //"Elkridge",
-            //"Relic",
-            //"dm01-entryway",
-            //"Exo",
+            "Shaar",
         };
 
         private static readonly ProtoId<EntityCategoryPrototype> DoNotMapCategory = "DoNotMap";
