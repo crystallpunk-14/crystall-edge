10664b152e95994b920c46adb0528bea15228b0e
Ed <96445749+TheShuEd@users.noreply.github.com>
2025-12-19 12:27:14 +0300

    Update CEClientBlueTextSystem.cs

--- Content.Client/_CE/Bluetext/CEClientBlueTextSystem.cs
+++ Content.Client/_CE/Bluetext/CEClientBlueTextSystem.cs
@@ -1,11 +1,15 @@
 using Content.Client.CharacterInfo;
 using Robust.Client.UserInterface.Controls;
 using Content.Shared._CE.BlueText;
+using Content.Shared.CCVar;
+using Robust.Shared.Configuration;
 
 namespace Content.Client._CE.BlueText;
 
 public sealed class CEClientBlueTextSystem : CESharedBlueTextSystem
 {
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+
     public override void Initialize()
     {
         base.Initialize();
@@ -15,6 +19,9 @@ public sealed class CEClientBlueTextSystem : CESharedBlueTextSystem
 
     private void OnGetCharacterInfoControls(ref CharacterInfoSystem.GetCharacterInfoControlsEvent ev)
     {
+        if (!_cfg.GetCVar(CCVars.CEGameShowBlueText))
+            return;
+
         if (!Mind.TryGetMind(ev.Entity, out var mind, out var mindComp))
             return;
 
