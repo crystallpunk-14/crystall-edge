c355d3f505a081fe30f17ac81b0ee7b4515a5e31
Ed <edwardxperia2000@gmail.com>
2025-12-23 21:19:44 +0300

    Update CEWorkbenchAutoCrafterComponent.cs

--- Content.Server/_CE/Workbench/Components/CEWorkbenchAutoCrafterComponent.cs
+++ Content.Server/_CE/Workbench/Components/CEWorkbenchAutoCrafterComponent.cs
@@ -11,33 +11,12 @@ using Robust.Shared.Prototypes;
 namespace Content.Server._CE.Workbench;
 
 /// <summary>
-/// This entity can be used to craft other objects through the interface
+///
 /// </summary>
 [RegisterComponent]
 [Access(typeof(CEWorkbenchSystem))]
 public sealed partial class CEAutoWorkbenchComponent : Component
 {
-    /// <summary>
-    /// Crafting speed modifier on this workbench.
-    /// </summary>
     [DataField]
-    public float CraftSpeed = 1f;
-
-    /// <summary>
-    /// List of recipes available for crafting on this type of workbench
-    /// </summary>
-    [DataField]
-    public List<ProtoId<CEWorkbenchRecipePrototype>> Recipes = new();
-
-    /// <summary>
-    /// Auto recipe list fill based on tags
-    /// </summary>
-    [DataField]
-    public List<ProtoId<TagPrototype>> RecipeTags = new();
-
-    /// <summary>
-    /// Played during crafting. Can be overwritten by the crafting sound of a specific recipe.
-    /// </summary>
-    [DataField]
-    public SoundSpecifier CraftSound = new SoundCollectionSpecifier("CEHammering");
+    public ProtoId<CEWorkbenchRecipePrototype>? SelectedRecipe;
 }
