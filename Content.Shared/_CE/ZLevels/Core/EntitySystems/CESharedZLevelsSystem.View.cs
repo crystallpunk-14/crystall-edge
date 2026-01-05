2185e35260b07844f031bed30ce59c760acbe874
Benjamin Schleiss <benjamin.schleiss@twofold.swiss>
2026-01-05 17:25:02 +0100

    added counter icons and counter alert

--- Content.Shared/_CE/ZLevels/Core/EntitySystems/CESharedZLevelsSystem.View.cs
+++ Content.Shared/_CE/ZLevels/Core/EntitySystems/CESharedZLevelsSystem.View.cs
@@ -5,21 +5,47 @@
 
 using Content.Shared._CE.ZLevels.Core.Components;
 using Content.Shared.Actions;
+using Content.Shared.Alert;
+using Content.Shared.Alert.Components;
 using Content.Shared.Maps;
 using Robust.Shared.Map;
+using Robust.Shared.Prototypes;
 
 namespace Content.Shared._CE.ZLevels.Core.EntitySystems;
 
 public abstract partial class CESharedZLevelsSystem
 {
     [Dependency] protected readonly ITileDefinitionManager TilDefMan = default!;
+    [Dependency] private readonly AlertsSystem _alert = default!;
+
     private void InitView()
     {
         SubscribeLocalEvent<CEZLevelViewerComponent, MoveEvent>(OnViewerMove);
         SubscribeAllEvent<ChangeViewedZLayerEvent>(OnChangeSelectedZLayer);
+        SubscribeLocalEvent<CEZLevelViewerComponent, CEViewedZLayerChangedEvent>(OnViewedZLayerChanged);
+
         SubscribeLocalEvent<CEZLevelViewerComponent, CEToggleZLevelLookUpAction>(OnToggleLookUp);
+        SubscribeLocalEvent<CEZLevelViewerComponent, ComponentStartup>(OnZLevelViewerStartup);
+        SubscribeLocalEvent<CEZLevelViewerComponent, GetGenericAlertCounterAmountEvent>(OnGetCounterAmount);
     }
 
+    private void OnZLevelViewerStartup(Entity<CEZLevelViewerComponent> ent, ref ComponentStartup args)
+    {
+        UpdateAlert(ent);
+    }
+
+    private void OnGetCounterAmount(Entity<CEZLevelViewerComponent> ent, ref GetGenericAlertCounterAmountEvent args)
+    {
+        if (args.Handled)
+            return;
+        if (args.Alert != ent.Comp.ZLayerAlert)
+            return;
+
+        var selected_z_layer = ent.Comp.ViewedZLevel;
+
+
+        args.Amount = Math.Abs(selected_z_layer);
+    }
     protected virtual void OnViewerMove(Entity<CEZLevelViewerComponent> ent, ref MoveEvent args)
     {
         if (!ent.Comp.LookUp)
@@ -70,6 +96,23 @@ public abstract partial class CESharedZLevelsSystem
         return !tileDef.Transparent;
     }
 
+    private void OnViewedZLayerChanged(Entity<CEZLevelViewerComponent> ent, ref CEViewedZLayerChangedEvent args)
+    {
+        CEZLayerAlertSeverity newSeverity;
+        if (ent.Comp.ViewedZLevel > 0) newSeverity = CEZLayerAlertSeverity.positive;
+        else if (ent.Comp.ViewedZLevel < 0) newSeverity = CEZLayerAlertSeverity.negative;
+        else newSeverity = CEZLayerAlertSeverity.neutral;
+
+        if (newSeverity == ent.Comp.ZLayerAlertSeverity) return;
+
+        ent.Comp.ZLayerAlertSeverity = newSeverity;
+        UpdateAlert(ent);
+    }
+    private void UpdateAlert(Entity<CEZLevelViewerComponent> target)
+    {
+        _alert.ShowAlert(target.Owner, target.Comp.ZLayerAlert, (short)target.Comp.ZLayerAlertSeverity);
+    }
+
     private void OnChangeSelectedZLayer(ChangeViewedZLayerEvent args)
     {
         if (args.Target is null) return;
@@ -87,6 +130,7 @@ public abstract partial class CESharedZLevelsSystem
     {
         ent.Comp.ViewedZLevel = value;
         DirtyField(ent, ent.Comp, nameof(CEZLevelViewerComponent.ViewedZLevel));
+        RaiseLocalEvent(ent, new CEViewedZLayerChangedEvent(value));
     }
     public bool TrySetViewedZLevel(Entity<CEZLevelViewerComponent> ent, int value)
     {
