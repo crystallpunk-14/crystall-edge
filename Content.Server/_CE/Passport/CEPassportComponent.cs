1534304cb05e2d8f358f30c8985fbed9ec360afc
Benjamin Schleiss <benjamin.schleiss@twofold.swiss>
2026-01-21 13:28:20 +0100

    ported passport from crystalpunk14

---
+++ Content.Server/_CE/Passport/CEPassportComponent.cs
@@ -0,0 +1,9 @@
+namespace Content.Server._CE.Passport;
+
+/// <summary>
+/// Generates a paper text confirming the identity of the player and stamps it.
+/// </summary>
+[RegisterComponent]
+public sealed partial class CEPassportComponent : Component
+{
+}
