c007840e9ad9fbfd04bf9e5e86e0f6821c7beb84
Ed <edwardxperia2000@gmail.com>
2026-01-25 00:08:35 +0300

    variation passes

---
+++ Content.Server/_CE/GameTicking/VariationPass/Components/ReplacementMarkers/CERockReplacementMarkerComponent.cs
@@ -0,0 +1,9 @@
+namespace Content.Server._CE.GameTicking.VariationPass.Components.ReplacementMarkers;
+
+/// <summary>
+/// This component marks replaceable rocks (stone walls) for use with fast queries in variation passes.
+/// </summary>
+[RegisterComponent]
+public sealed partial class CERockReplacementMarkerComponent : Component
+{
+}
