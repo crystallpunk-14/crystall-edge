523c576c97ab88244b051901b55bd1b8ffd41dec
Ed <edwardxperia2000@gmail.com>
2026-07-17 12:49:40 +0300

    Update CCvars.CEMisc.cs

--- Content.Shared/_CE/CCVars/CCvars.CEMisc.cs
+++ Content.Shared/_CE/CCVars/CCvars.CEMisc.cs
@@ -1,3 +1,5 @@
+using Content.Shared.Administration;
+using Content.Shared.CCVar.CVarAccess;
 using Robust.Shared.Configuration;
 
 namespace Content.Shared.CCVar;
@@ -7,6 +9,7 @@ public sealed partial class CCVars
     /// <summary>
     ///     Whether the arrivals shuttle is enabled.
     /// </summary>
+    [CVarControl(AdminFlags.Admin)]
     public static readonly CVarDef<bool> CEArrivalsShuttles =
         CVarDef.Create("shuttle.ce_arrivals", true, CVar.SERVERONLY);
 
@@ -19,18 +22,21 @@ public sealed partial class CCVars
     /// <summary>
     /// Automatically shuts down the server outside of the CBT plytime. Shitcoded enough, but it's temporary anyway
     /// </summary>
+    [CVarControl(AdminFlags.Admin)]
     public static readonly CVarDef<bool> CEClosedBetaTest =
         CVarDef.Create("game.closed_beta_test", false, CVar.SERVERONLY);
 
     /// <summary>
     ///     Controls if round-end window shows custom objective summary for antags
     /// </summary>
+    [CVarControl(AdminFlags.Admin)]
     public static readonly CVarDef<bool>
         CEGameShowBlueText = CVarDef.Create("game.showbluetext", true, CVar.ARCHIVE | CVar.REPLICATED);
 
     /// <summary>
     ///     URL of the Discord webhook which will relay round end summary messages.
     /// </summary>
+    [CVarControl(AdminFlags.Admin)]
     public static readonly CVarDef<string> CEDiscordRoundEndSummaryWebhook =
         CVarDef.Create("discord.round_end_summary_webhook", string.Empty, CVar.SERVERONLY | CVar.CONFIDENTIAL);
 }
