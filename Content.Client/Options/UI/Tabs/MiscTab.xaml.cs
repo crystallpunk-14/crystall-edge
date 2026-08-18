3636d07b5eb8dde4990d697ab471b9db05a9eac3
TheShuEd <96445749+TheShuEd@users.noreply.github.com>
2026-08-18 19:46:40 +0300

    UI fixes (#417)
    * ui fixes
    
    * restore separated
    
    * ignore vanlla tests

--- Content.Client/Options/UI/Tabs/MiscTab.xaml.cs
+++ Content.Client/Options/UI/Tabs/MiscTab.xaml.cs
@@ -51,7 +51,8 @@ public sealed partial class MiscTab : Control
         Control.AddOptionCheckBox(CCVars.OpaqueStorageWindow, OpaqueStorageWindowCheckBox);
         Control.AddOptionCheckBox(CCVars.ChatEnableFancyBubbles, FancySpeechBubblesCheckBox);
         Control.AddOptionCheckBox(CCVars.ChatFancyNameBackground, FancyNameBackgroundsCheckBox);
-        Control.AddOptionCheckBox(CCVars.StaticStorageUI, StaticStorageUI);
+        // CrystallEdge: storage windows are always free-floating, hide this switch
+        // Control.AddOptionCheckBox(CCVars.StaticStorageUI, StaticStorageUI);
         Control.AddOptionCheckBox(CCVars.InterfaceChatFollowButton, ChatFollowButton);
 
         Control.Initialize();
